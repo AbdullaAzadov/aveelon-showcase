a8ae6986e1a9288d59fe19bc5d5781c47b9e635d
AbdullaAzadov <123924334+AbdullaAzadov@users.noreply.github.com>
2024-12-29 21:08:33 +0500

    fix 7

---
+++ assets/OfferPage-D1dK6CN9.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-gPAzqeJY.js";import{O as t}from"./Info-BGW0W7q7.js";import{m as a}from"./index-XI9iT2e2.js";const m=()=>r.jsx(a,{data:t});export{m as OfferPage,m as default};
